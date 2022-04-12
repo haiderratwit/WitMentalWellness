69c7989d9b483c2d431fe2ac569719832b79cb5b
Dai Van <91207317+vandatwit@users.noreply.github.com>
2022-04-12 18:17:18 -0400

    -Added counselor scheduling -Confirm Email -Retrieve the username email for the confirmation email - Scheduling with counselor choice

--- app/src/main/java/edu/wit/mobileapp/wellness_app/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/edu/wit/mobileapp/wellness_app/ui/dashboard/DashboardFragment.java
@@ -1,23 +1,37 @@
 package edu.wit.mobileapp.wellness_app.ui.dashboard;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import java.io.FileOutputStream;
+
+import edu.wit.mobileapp.wellness_app.R;
 import edu.wit.mobileapp.wellness_app.databinding.FragmentDashboardBinding;
+import edu.wit.mobileapp.wellness_app.ui.notifications.NotificationsFragment;
 
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
     private FragmentDashboardBinding binding;
 
+    private String fileName = "Wellness_Counselor";
+    private String counselorName ="";
+    FileOutputStream outputStream;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         dashboardViewModel =
@@ -49,12 +63,82 @@ public class DashboardFragment extends Fragment {
         });
 
 
+        TextView counselor_1 = binding.counselorName1;
+        counselor_1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                FragmentManager fm = getActivity().getSupportFragmentManager();
+                FragmentTransaction transaction = fm.beginTransaction();
+                Fragment fragment_home = new NotificationsFragment();
+                transaction.replace(R.id.container, fragment_home);
+                transaction.commit();
+                counselorName = counselor_1.getText().toString();
+
+                FileOutputStream outputStream;
+                try{
+                    outputStream = getActivity().openFileOutput(fileName, Context.MODE_PRIVATE);
+                    outputStream.write(counselorName.getBytes());
+                    outputStream.close();
+                }catch (Exception e){
+                    Log.v("Write counselor name","Error: " + e);
+                }
+            }
+        });
+
+        TextView counselor_2 = binding.counselorName2;
+        counselor_2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                FragmentManager fm = getActivity().getSupportFragmentManager();
+                FragmentTransaction transaction = fm.beginTransaction();
+                Fragment fragment_home = new NotificationsFragment();
+                transaction.replace(R.id.container, fragment_home);
+                transaction.commit();
+                counselorName = counselor_2.getText().toString();
+
+                FileOutputStream outputStream;
+                try{
+                    outputStream = getActivity().openFileOutput(fileName, Context.MODE_PRIVATE);
+                    outputStream.write(counselorName.getBytes());
+                    outputStream.close();
+                }catch (Exception e){
+                    Log.v("Write counselor name","Error: " + e);
+                }
+            }
+        });
+
+        TextView counselor_3 = binding.counselorName1;
+        counselor_3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                FragmentManager fm = getActivity().getSupportFragmentManager();
+                FragmentTransaction transaction = fm.beginTransaction();
+                Fragment fragment_home = new NotificationsFragment();
+                transaction.replace(R.id.container, fragment_home);
+                transaction.commit();
+                counselorName = counselor_3.getText().toString();
+
+                FileOutputStream outputStream;
+                try{
+                    outputStream = getActivity().openFileOutput(fileName, Context.MODE_PRIVATE);
+                    outputStream.write(counselorName.getBytes());
+                    outputStream.close();
+                }catch (Exception e){
+                    Log.v("Write counselor name","Error: " + e);
+                }
+            }
+        });
+
+
+
         return root;
     }
+
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
+
 }
--- app/src/main/java/edu/wit/mobileapp/wellness_app/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/edu/wit/mobileapp/wellness_app/ui/notifications/NotificationsFragment.java
@@ -1,5 +1,6 @@
 package edu.wit.mobileapp.wellness_app.ui.notifications;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,6 +19,12 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import edu.wit.mobileapp.wellness_app.R;
 import edu.wit.mobileapp.wellness_app.databinding.FragmentNotificationsBinding;
 import edu.wit.mobileapp.wellness_app.ui.home.HomeFragment;
@@ -27,6 +34,10 @@ public class NotificationsFragment extends Fragment {
     private NotificationsViewModel notificationsViewModel;
     private FragmentNotificationsBinding binding;
 
+    private String fileName = "Wellness_Counselor";
+    private Context context;
+    private String counselor_choice ="";
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         notificationsViewModel =
@@ -34,6 +45,29 @@ public class NotificationsFragment extends Fragment {
 
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        context = getActivity().getApplicationContext();
+        FileInputStream fis;
+        InputStreamReader isr;
+        BufferedReader bufferedReader;
+
+        try{
+            fis = context.openFileInput(fileName);
+            isr = new InputStreamReader(fis);
+            bufferedReader = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while((line = bufferedReader.readLine())!=null){
+                sb.append(line);
+                counselor_choice = sb.toString();
+            }
+        }catch(FileNotFoundException e){
+            Log.v("Read File: ", "Error: " + e);
+        } catch(IOException e){
+            Log.v("Read File: ", "Error: " + e);
+        }
+
+        Log.v("Email receive", "Email is: " + counselor_choice);
 
         final CalendarView calendarView = binding.calendarView;
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
@@ -44,12 +78,13 @@ public class NotificationsFragment extends Fragment {
                 bundle.putInt("Year", year);
                 bundle.putInt("Month", month+1);
                 bundle.putInt("Day", day);
+                bundle.putString("CounselorName", counselor_choice);
                 Intent time_schedule = new Intent(getActivity(), edu.wit.mobileapp.wellness_app.ui.notifications.time_schedule.class);
                 time_schedule.putExtras(bundle);
                 startActivity(time_schedule);
             }
         });
-
+        /*
         final Button back_button = binding.backButton1;
         back_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,7 +95,7 @@ public class NotificationsFragment extends Fragment {
                 transaction.replace(R.id.container, fragment_home);
                 transaction.commit();
             }
-        });
+        });*/
         return root;
     }
 
