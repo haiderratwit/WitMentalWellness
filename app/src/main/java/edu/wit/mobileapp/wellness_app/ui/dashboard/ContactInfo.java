71edf008ae198f6e799fae356ac8747ee5031777
Rida Haider <haiderr@wit.edu>
2022-04-12 15:58:09 -0400

    Added counselor bio page and contact info page in dashboard

---
+++ app/src/main/java/edu/wit/mobileapp/wellness_app/ui/dashboard/ContactInfo.java
@@ -0,0 +1,33 @@
+package edu.wit.mobileapp.wellness_app.ui.dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import edu.wit.mobileapp.wellness_app.R;
+
+public class ContactInfo extends AppCompatActivity {
+
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_info2);
+
+        button = (Button) findViewById(R.id.back2CounselorBtn);
+        button.setOnClickListener(new View.
+                OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                backToCounselor2();
+            }
+        });
+    }
+    public void backToCounselor2(){
+        Intent intent = new Intent(this, DashboardFragment.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/edu/wit/mobileapp/wellness_app/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/edu/wit/mobileapp/wellness_app/ui/dashboard/DashboardFragment.java
@@ -1,18 +1,16 @@
 package edu.wit.mobileapp.wellness_app.ui.dashboard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import edu.wit.mobileapp.wellness_app.R;
 import edu.wit.mobileapp.wellness_app.databinding.FragmentDashboardBinding;
 
 public class DashboardFragment extends Fragment {
@@ -28,13 +26,29 @@ public class DashboardFragment extends Fragment {
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//        final TextView textView = binding.textView;
+//        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+
+        Button button = binding.contactBtn;
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+
+
+
+                Intent intent = new Intent(getActivity(), edu.wit.mobileapp.wellness_app.ui.dashboard.ContactInfo.class);
+
+                startActivity(intent);
+
             }
         });
+
+
         return root;
     }
 
